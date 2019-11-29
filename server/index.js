aa6b2b64f5af05c5eaeab8a997b30fc382c3b0ee
Carlos Neto <carloshtcursos@gmail.com>
2019-11-29 12:33:27 -0400

    Teste preliminar com jqueri

--- server/index.js
+++ server/index.js
@@ -13,6 +13,12 @@ APP.set('views engine', 'html');
 APP.use('/', (req, res) => {
   res.render('index.html');
 });
+SOCKET_IO.on('connection', socket => {
+  console.log(`Usuario ${socket.id} conectado`);
+  socket.on('sendMessage', data => {
+    console.log(data);
+  });
+});
 const PORT = process.env.PORT;
 SERVER.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
