794fd80ee963b42b9767f62a93a0da72554a64f0
Carlos Neto <carloshtcursos@gmail.com>
2019-11-29 11:52:55 -0400

    Criando arquivo de configuracao

--- server/index.js
+++ server/index.js
@@ -2,10 +2,18 @@ const EXPRESS = require('express');
 const PATH = require('path');
 const APP = EXPRESS();
 const SERVER = require('http').createServer(APP);
+require('./../configuration');
 // eslint-disable-next-line no-unused-vars
 const SOCKET_IO = require('socket.io')(SERVER);
 
 APP.use(EXPRESS.static(PATH.join(__dirname, 'client')));
-APP.set('views', PATH.join(__dirname, 'public'));
+APP.set('views', PATH.join(__dirname, 'client'));
 APP.engine('html', require('ejs').renderFile);
 APP.set('views engine', 'html');
+APP.use('/', (req, res) => {
+  res.render('index.html');
+});
+const PORT = process.env.PORT;
+SERVER.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
